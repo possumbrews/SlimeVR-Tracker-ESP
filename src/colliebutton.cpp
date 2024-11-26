90f5625a2dc09298eb9f6d753e8af58e325573e8
QuestionMKII <Skulblaka@yahoo.com>
2024-11-26 10:45:58 -0800

    fixed button delay

--- src/colliebutton.cpp
+++ src/colliebutton.cpp
@@ -47,8 +47,9 @@ namespace collieButton {
 
                                            // These variables are remembered across loops
     bool button_FirstRelease = true;          // We'll ignore the first release of button
-     int button_IsDown = true;            // So we'll know if the button state is changed
+    int button_IsDown = true;            // So we'll know if the button state is changed
     long button_TimeWhenPressedDown = 0;         // So we'll know how long it was pressed
+    bool justStarted = true;
 
     void setup() {
         pinMode(BUTTON_PIN, INPUT_PULLUP);                     // Sets button pin as input
@@ -62,44 +63,47 @@ namespace collieButton {
     void update() {        // In main's loop, this is the colliebutton::update() we call!
 
                                                    // These variables are reset each loop
-            currentTime = millis();
-        int buttonState = digitalRead(BUTTON_PIN);
-        
-        if (currentTime > STARTUP_DELAY) {           // Button is 100% ignored for first 2 seconds
-            if (buttonState != button_IsDown) {       // If button's state has changed...
-                if (button_FirstRelease == true) {            // Ignore the first release
-                    button_FirstRelease = false;
-                } 
-                else {
-                    button_IsDown = buttonState;             // Record the button's state
-                    if (button_IsDown == HIGH) {               // If button is pressed...
-                        button_TimeWhenPressedDown = millis();   // Record the press time
-                    } 
-                    else {                            // Else, if button is released...
-                        if ((currentTime - button_TimeWhenPressedDown) >= SHUTDOWN_TRIGGER) {      // If button was held for more than 3 seconds...
-                                                          
-                                          // INSERT SHUTDOWN CODE HERE c:
+             currentTime = millis();
+         int buttonState = digitalRead(BUTTON_PIN);
 
-                          while (69 == 69) {                                                  // Enter SHUTDOWN !!!
+        if (justStarted == true) {
+            if (currentTime > STARTUP_DELAY) {      // Button is 100% ignored for first 2 seconds
+                justStarted = false;
+            }
+        } else {
+
+            if (buttonState != button_IsDown) {                  // If button's state has changed...
+                button_IsDown = buttonState;                 // Record the button's state
+                if (button_IsDown == LOW) {                  // If button is pressed...
+                    button_TimeWhenPressedDown = millis();   // Record the press time
+                }
+            }
+
+            if (buttonState == LOW && (currentTime - button_TimeWhenPressedDown) >= SHUTDOWN_TRIGGER) {
+                // If button was held for more than 3 seconds...
+
+                                        // INSERT SHUTDOWN CODE HERE c:
+
+                        while (69 == 69) {                                                    // Enter SHUTDOWN !!!
                             buttonState = digitalRead(BUTTON_PIN);                            // Check if button is pressed
                             digitalWrite(LED_PIN, LOW);                                       //==========================================
                             delay(69);                                                        //===============FLASH======================
                             digitalWrite(LED_PIN, HIGH);                                      //================LED=======================
                             delay(69);                                                        //==========================================
-                            if (buttonState == HIGH) {                                        // Check if button is relesed
-                              delay(1000);
-                              pinMode(BUTTON_PIN, OUTPUT);                                    // Set output to turn off inturnal pull up resistor.
-                              digitalWrite(BUTTON_PIN, LOW);                                  // Turn off device
-                              delay(3000);                                                    // Delay to alow caps to drain
-                              break;                                                          // EXIT SUTDOWN if ESP is still receving power                               
+                            if (buttonState == HIGH) {                                        // Check if button is released
+                                delay(1000);
+                                pinMode(BUTTON_PIN, OUTPUT);                                  // Set output to turn off inturnal pull up resistor.
+                                digitalWrite(BUTTON_PIN, LOW);                                // Turn off device
+                                delay(3000);                                                  // Delay to alow caps to drain
+                                break;                                                        // EXIT SUTDOWN if ESP is still receving power
                             }
-                          } 
                         }
-                      }
-                  }
             }
+
         }
     }
+
+
 }
 // //=============================================
 // //   ----- end / Tyroie's Backup code -----
